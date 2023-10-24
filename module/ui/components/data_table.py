8cffb73594b45e97c8d1162c295067fd81dd6cb2
YuNakas <metal.edge.core.ease@gmail.com>
2023-10-24 23:46:18 +0900

    メソッド構成の見直し

--- module/ui/components/data_table.py
+++ module/ui/components/data_table.py
@@ -1,23 +1,23 @@
 import flet as ft
 
 def create_data_table(strings, click_callback):
+    def create_rows(): 
+        rtnRows = []
+        for string in strings: 
+            rtnRows.append(ft.DataRow(
+                cells=[ft.DataCell(ft.Text(string))],
+                on_select_changed=lambda e: click_callback(e.control.cells[0].content.value)
+            ))
+        return rtnRows
+    
     table = ft.DataTable(
         columns=[
             ft.DataColumn(ft.Text("File Name"))
         ],
-        rows=create_rows(strings, click_callback),
+        rows=create_rows(),
     )
 
     # スクロール可能にする
     lv = ft.ListView(expand=1)
     lv.controls.append(table)
     return lv
-
-def create_rows(strings, click_callback): 
-    rtnRows = []
-    for string in strings: 
-        rtnRows.append(ft.DataRow(
-            cells=[ft.DataCell(ft.Text(string))],
-            on_select_changed=lambda e: click_callback(e.control.cells[0].content.value)
-        ))
-    return rtnRows
