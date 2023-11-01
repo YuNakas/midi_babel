0ec4f5a3ccb52580af650f034c7101021c5a8c1e
YuNakas <metal.edge.core.ease@gmail.com>
2023-11-01 22:49:26 +0900

    フォント追加

--- midi_babel.py
+++ midi_babel.py
@@ -8,6 +8,10 @@ def main(page: ft.Page):
     page.window_width = 840
     page.window_height = 800
     create_view.create_view(page)
+    page.fonts = {
+        "NotoSansJP": "/fonts/NotoSansJP-Regular.otf"
+    }
+    page.theme = ft.Theme(font_family="NotoSansJP")
 
 if __name__ == '__main__':
     # 初期化処理
@@ -15,4 +19,4 @@ if __name__ == '__main__':
     _init.init_state()
     _init.init_midi()
     
-    ft.app(target=main)
+    ft.app(target=main, assets_dir="assets")
