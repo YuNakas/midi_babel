5e5ac70335c232f8cd82270fcbc774c58de480c1
YuNakas <metal.edge.core.ease@gmail.com>
2023-11-04 17:09:06 +0900

    mappingの削除

--- module/classes/conf_class.py
+++ module/classes/conf_class.py
@@ -3,7 +3,6 @@ from module.util import string_util
 
 class ConfClass():
     __root_path: str
-    __mapping_files: list[str]
     __key_mapping_files: list[str]
     __convert_midi_files: list[str]
     __converted_midi_files: list[str]
@@ -11,7 +10,6 @@ class ConfClass():
 
     def __init__(self) -> None:
         self.__root_path = str(pathlib.Path('__file__').resolve().parent)
-        self.__mapping_files = string_util.get_filename(os.listdir(self.__root_path + '/mapping'), "yml")
         self.__key_mapping_files = string_util.get_filename(os.listdir(self.__root_path + '/assets/key_mapping'), "yml")
         self.__convert_midi_files = string_util.get_filename(os.listdir(self.__root_path + '/assets/midi_ready'), "mid")
         self.__converted_midi_files = string_util.get_filename(os.listdir(self.__root_path + '/assets/midi_converted'), "mid")
@@ -19,9 +17,6 @@ class ConfClass():
     
     def get_root_path(self):
         return self.__root_path
-    
-    def get_mapping_file(self):
-        return self.__mapping_files
     
     def get_key_mapping_files(self):
         return self.__key_mapping_files
